798c73d4db3c90a7ec41cc39e19b046e27ffb672
Mary A Green <cyberangel9911@gmail.com>
2021-10-05 18:38:29 -0500

    firebase
    added firebased

---
+++ components/AmazonHome.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+export default function AmazonHome() {
+    return (<h1>Start Amazon page!</h1>
+
+
+    )};
---
+++ utils/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCHia4rOWYPAnszOWHeD-jbzKctGmZs-LA",
+    authDomain: "project-91a9b.firebaseapp.com",
+    projectId: "project-91a9b",
+    storageBucket: "project-91a9b.appspot.com",
+    messagingSenderId: "867277071107",
+    appId: "1:867277071107:web:dd675cd225858b08553f36",
+    measurementId: "G-PYBMF3WW0P"
+  };
